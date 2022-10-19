2c171d9e3f5cf75c2cc763508b2cdff3510dc9d1
Blint18 <balintlike@citromail.hu>
2022-10-19 17:00:27 +0200

    script.js added

---
+++ script.js
@@ -0,0 +1,42 @@
+// console.log("hello world")
+// console.log(12)
+// console.log(false)
+
+const var1 = "var1"
+const var2 = 5
+const var3 = "5"
+
+// console.log("var1")
+// console.log(var1)
+// console.log(var2)
+// console.log(var3)
+
+// console.log(var2*2)
+// console.log(var3*2)
+
+const myArray = [
+    "balint",
+    24,
+    var1
+]
+
+// console.log(myArray)
+// console.log(myArray[0])
+// console.log(myArray[1])
+// console.log(myArray[2])
+
+const myObject = {
+    key1: "value",
+    key2: 69,
+    key3: var3,
+    arrayKey: [
+        var1,
+        var2,
+        true
+    ]
+}
+
+console.log(myObject)
+console.log(myObject.key1)
+console.log(myObject.key2)
+console.log(myObject.arrayKey[0])
